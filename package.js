9d23299ad1a1475789d03818d4768eb14675f4de
dcsan <dc@pikkle.com>
2014-10-17 22:48:49 -0700

    mods to work with new package manager

--- package.js
+++ package.js
@@ -1,18 +1,25 @@
 Package.describe({
-  summary: "Easily include ace, receive reactive varibles for cursor position, editor contents, etc"
+  description: "Easily include ace, receive reactive varibles for cursor position, editor contents, etc",
+  "version": "0.0.3",
+  "name": "dcsan:reactive-ace",
 });
-
 
 var bundlerApi = null
 var path = Npm.require("path");
 var fs = Npm.require("fs");
 var packagePath = path.join(path.resolve("."), "packages", "reactive-ace");
 
+// console.log("packagePath:", packagePath);
+// console.log("dirName:", __dirname);
+// console.log("cwd:", path.resolve(".") );
+// console.log(require.main.filename)
+
 Package.on_use(function (api, where) {
   api.use(['jquery', "templating", "coffeescript", "underscore", "deps"], ["client"]);
 
   var files = fs.readdirSync(path.join(packagePath, "ace-builds", "src"));
   files.forEach(function(file){
+    // console.log("loading file", file)
     if (file === "snippets"){return;}
     api.add_files(path.join("ace-builds", "src", file), "client", {isAsset: true});
   });
